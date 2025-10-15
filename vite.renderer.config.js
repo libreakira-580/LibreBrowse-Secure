f339794a85b7bbe9a610847279fb72fde8f9ace4
libreakira-580 <a21699844@gmail.com>
2025-10-15 21:08:19 +0100

    re made it as the old version which was amnesciac
    did what title said not much to it

--- vite.renderer.config.js
+++
@@ -1,9 +0,0 @@
-
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-export default defineConfig({
-  root: '.',
-  plugins: [svelte()],
-  build: { outDir: 'dist', rollupOptions: { input: '/src/renderer/index.html' } }
-});
